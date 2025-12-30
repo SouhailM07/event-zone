fa2a0d07a330ecc733900d45be2bcf531065516b
Nazim <nazimfr4@gmail.com>
2025-12-30 13:54:29 +0100

    Tasks done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return "Cool";
+})->name('login');
+
+Route::get('/logout', function () {
+    return "You Loged Out";
+})->name('logout');
+
+// Rigester Route
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+// Logout Route
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/profile', [AuthController::class, 'profile'])
+->name('profile')
+->middleware('auth');
